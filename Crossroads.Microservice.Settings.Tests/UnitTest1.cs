7d80ea481f95581608834fcdabafa10c494df9bd
dillon-courts <dillon.courts@gmail.com>
2019-03-06 08:39:01 -0500

    Some unit tests

--- Crossroads.Microservice.Settings.Tests/UnitTest1.cs
+++ Crossroads.Microservice.Settings.Tests/UnitTest1.cs
@@ -7,12 +7,34 @@ namespace Crossroads.Microservice.Settings.Tests
     {
 
         [Fact]
-        public void GetSecret_NullKey()
+        public void GetSecret_NullKey_ThrowsException()
         {
             var service = new SettingsService();
 
             //TODO: 
             Assert.ThrowsAny<Exception>(() => service.GetSecret(null));
         }
+
+        [Fact]
+        public void GetSecret_EmptyStringKey_ThrowsException()
+        {
+            
+        }
+
+        [Fact]
+        public void GetSecret_NoKeyInSecrets_ReturnsNull()
+        {
+            var service = new SettingsService();
+
+            var secret = service.GetSecret("THIS_KEY_DOES_NOT_EXIST");
+
+            Assert.Null(secret);
+        }
+
+        //string GetSecret(string key);
+        //bool TryGetSecret(string key, out string secret);
+
+        //void AddSettings(Dictionary<string, string> settings, string source);
+        //void AddSetting(string key, string value, string source);
     }
 }
